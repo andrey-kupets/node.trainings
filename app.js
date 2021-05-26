c162a4d014ccae361aa0a3672975e2bfffa182b9
andrey-kupets <andrey.kupets@i.ua>
2021-05-26 21:26:44 +0300

    add hbs' form

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
 const express = require('express');
+const expressHbs = require('express-handlebars');
 
-// const file = require('./dir/file');
+// const file = require('./static/file');
 // console.log(file); // log WHOLE file = const + log of required file
 
 const fs = require('fs');
 const path = require('path');
 const { config: { PORT } } = require('./config');
 
-// ------------- Fs, Stream ------------------
-// const filePath = path.join(process.cwd(), 'dir', 'file.txt');
+// ------------- 1. Fs. Stream ------------------
+// const filePath = path.join(process.cwd(), 'static', 'file.txt');
 //
 // fs.writeFile(filePath, 'JUSTICE', (err) => {
 //     if (err) console.log(err);
 // });
 //
-// fs.writeFile(process.cwd() + '/dir/file.csv', 'dust', (err) => {
+// fs.writeFile(process.cwd() + '/static/file.csv', 'dust', (err) => {
 //     if (err) console.log(err);
 // });
 //
 //
 // const readStream = fs.createReadStream(filePath);
-// const writeStream = fs.createWriteStream(path.join(process.cwd(), '/dir', '/king.txt'));
+// const writeStream = fs.createWriteStream(path.join(process.cwd(), '/static', '/king.txt'));
 // const writeStream = fs.createWriteStream(filePath); // make a file void anew
 //
 // for (let i = 0; i < 100000; i++) {
@@ -43,10 +44,18 @@ const { config: { PORT } } = require('./config');
 //or
 // readStream.pipe(writeStre
 
-// ------------- Express --------------------
+// ------------- 2. Express. Hbs --------------------
 const app = express();
+
+app.use(express.static(path.join(process.cwd(), 'static')));
+app.set('view engine', '.hbs');
+app.engine('.hbs', expressHbs({ defaultLayout: false }));
+app.set('views', path.join(process.cwd(), 'static'));
+
+app.get('/login',(req, res) => {
+    res.render('login');
+})
+
 app.listen(PORT, () => {
-    console.log(`Port ${PORT} is being listened`)
+    console.log(`Port ${PORT} is being listened`);
 });
-
-
